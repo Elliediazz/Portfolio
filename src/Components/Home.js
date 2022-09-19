9a70b4e84cf4e42ed89f23e3f94f8e1e02ae4f6e
Ellie Diaz <elliediaz@Ellies-Air.attlocal.net>
2022-09-19 18:38:26 -0400

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../Styling/Components.css'
+
+export default function Home(){
+    return(
+        <div>
+            <h3>Hello! My name is </h3>
+            <h1>Ellie Barrera-Diaz</h1>
+            <p>Im a Software developer in the making! 
+                Thus far, I have specialized in building web aplications using Javascript 
+                and a little bit of Python. 
+            </p>
+        </div>
+    )
+}
