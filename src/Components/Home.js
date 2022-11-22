2e4cb4e81b20edb6bdb052efc4771e7dbb597b4c
Ellie Barrera <elliediaz1204@gmail.com>
2022-11-21 21:57:42 -0500

    chnaged color scheme and redid links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,27 +4,15 @@ import ProfilePic from "../Styling/ProfilePic.jpeg";
 
 export default function Home(){
     return(
-        <div className="all-bg">
-          <div className="home-flex-container">
-            <div className="home">
-              <h4 className="greeting">Welcome, my name is </h4>
-              <h1 className="name">Ellie Barrera</h1>
-              <div className="home-content-div">
-              <p className="home-content"> Im a Junior Software Developer seeking 
-                  to pursue an opportunity with a growing organization that will 
-                  utilize and enhance my knowledge in software development
-              </p>
-              <p className="home-content"> Some Languages, Databases and Frameworks 
-                  I have worked with include: Javascript, Python, React.js, Express, Node.js, 
-                  SQL, PostgreSQL, Mongoose, and MongoDB
-              </p>
-              <p className="home-content"> Keep reading to checkout some of the projects 
-                I have worked on!
-              </p>
+        <div className="all-bg home-flex-container">
+            <div className="home-content">
+              <h1 className="greeting">Welcome </h1>
+              <h1 className="name">My Name is Ellie Barrera</h1>
+              <div className="home-info-div">
+              
               </div>
             </div>
             <img className="profile" src={ProfilePic} alt="Profile"/>
-          </div>
         </div>
     )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../Styling/Components.css'
 import PDF from "../Styling/Resume.pdf"
-import logo from "../Styling/logo.png"
+import logo from "../Styling/PersonalLogo.png"
+import linkedInLogo from "../Styling/linkedInLogo.png"
+import githubLogo from "../Styling/githubLogo.png"
+import twitterLogo from "../Styling/twitterLogo.png"
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -10,16 +13,21 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NavBar() {
     return(
         <div>
-            <Navbar bg="light" variant="light" fixed="top">
+            <Navbar fixed="top">
         <Container >
           <Navbar.Brand className="navbar-brand" href="/">< img src={logo} alt="Logo" className='logo'/></Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/experience">Experience</Nav.Link>
             <Nav.Link href="/projects">Projects</Nav.Link>
+            {/* <Nav.Link href="/education">Education</Nav.Link> */}
+            <Nav.Link href="/experience">Experience</Nav.Link>
             <Nav.Link href="/contact">Contact</Nav.Link>
             <Nav.Link target="_blank" rel="noopener noreferrer" href={PDF}>Resume</Nav.Link>
+            <div className='icons'>
+              <Navbar.Brand className="navbar-brand" href="https://www.linkedin.com/in/elliediaz/">< img src={linkedInLogo} alt="LinkedIn Page" className='linkedinicon'/></Navbar.Brand>
+              <Navbar.Brand className="navbar-brand" href="https://github.com/Elliediazz">< img src={githubLogo} alt="Github Page" className='icon'/></Navbar.Brand>
+              <Navbar.Brand className="navbar-brand" href="https://twitter.com/diazzellie">< img src={twitterLogo} alt="Twitter Page" className='icon'/></Navbar.Brand>
+            </div>
           </Nav>
         </Container>
       </Navbar>
