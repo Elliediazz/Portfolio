424b433794b504635c533f303192cadcb7130cc2
Ellie Barrera <elliediaz1204@gmail.com>
2022-11-30 15:02:42 -0500

    finished skills UI

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -17,26 +17,58 @@ export default function Skills(){
             <div className="header-div">
                 <h1 className="header">Languages & Skills</h1>
             </div>
+           <div className="skills-detail">
+                <h1>What I Am Great At</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu nunc erat. Vestibulum lacinia ligula sit amet lorem euismod, quis feugiat metus ornare. Curabitur a justo eleifend, pellentesque augue eu, bibendum enim. Sed vitae neque vestibulum, efficitur justo sed, lacinia nunc. Nam fermentum cursus orci, quis hendrerit ligula ornare at. Suspendisse luctus volutpat nisi. Phasellus congue mi augue, vel tempor mauris porta id.</p>
+           </div>
             <div className="skills-info-container">
                 <div className="horizontal-scroll">
                     <button className="btn-scroll" id="btn-scroll-left" ><ion-icon name="chevron-back-outline"></ion-icon></button>
                     <button className="btn-scroll" id="btn-scroll-right"><ion-icon name="chevron-forward-outline"></ion-icon></button>
                     <div className="skills-container">
-                        <div className="skills"><img src={ReactIcon} alt="React Icon"/></div>
-                        <div className="skills"><img src={NodeIcon} alt="Node Icon"/></div>
-                        <div className="skills"><img src={PythonIcon} alt="Python Icon"/></div>
-                        <div className="skills"><img src={CssIcon} alt="Css Icon"/></div>
-                        <div className="skills"><img src={HtmlIcon} alt="Html Icon"/></div>
-                        <div className="skills"><img src={JSIcon} alt="JS Icon"/></div>
-                        <div className="skills"><img src={MongoIcon} alt="Mongo Icon"/></div>
-                        <div className="skills"><img src={PostgresIcon} alt="Postgres Icon"/></div>
-                        <div className="skills"><img src={ExpressIcon} alt="Express Icon"/></div>
-                        <div className="skills"><img src={SQLIcon} alt="SQL Icon"/></div>
+                        <div>
+                            <div className="skills"><img src={ReactIcon} alt="React Icon"/></div>
+                            <h1>React</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={NodeIcon} alt="Node Icon"/></div>
+                            <h1>Node</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={PythonIcon} alt="Python Icon"/></div>
+                            <h1>Python</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={CssIcon} alt="Css Icon"/></div>
+                            <h1>CSS</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={HtmlIcon} alt="Html Icon"/></div>
+                            <h1>HTML</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={JSIcon} alt="JS Icon"/></div>
+                            <h1>Javascript</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={MongoIcon} alt="Mongo Icon"/></div>
+                            <h1>MondoDB</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={PostgresIcon} alt="Postgres Icon"/></div>
+                            <h1>PostgreSQL</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={ExpressIcon} alt="Express Icon"/></div>
+                            <h1>Express</h1>
+                        </div>
+                        <div>
+                            <div className="skills"><img src={SQLIcon} alt="SQL Icon"/></div>
+                            <h1>SQL</h1>
+                        </div>
                     </div>
                 </div>
-                
             </div>
-
         </div>
     )
 }
