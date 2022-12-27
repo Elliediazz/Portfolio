ef6d2f8ff0422c8559ef332f60e2066e603a250a
Ellie Barrera <elliediaz1204@gmail.com>
2022-12-27 14:45:14 -0500

    fixed links

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -8,17 +8,17 @@ export default function Experience() {
     return(
       <div className="all-bg experience-flex-container">
         <div className="experience-title">
-            <button onClick={()=> setActive("CardOne")}>University Of Wisonsin- Madison</button>
-            <button onClick={()=> setActive("CardTwo")}>Jen-Ter Technologies</button>
-            <button onClick={()=> setActive("CardThree")}>Jen-Ter Technologies - Internship</button>
-            <button onClick={()=> setActive("CardFour")}>University Of Wisonsin- Oshkosh</button>
+          <button onClick={()=> setActive("CardOne")}>University Of Wisonsin- Madison</button>
+          <button onClick={()=> setActive("CardTwo")}>Jen-Ter Technologies</button>
+          <button onClick={()=> setActive("CardThree")}>Jen-Ter Technologies - Internship</button>
+          <button onClick={()=> setActive("CardFour")}>University Of Wisonsin- Oshkosh</button>
         </div> 
         <div className="experience-details">
             <div>
-                {active === "CardOne" && <Card CardData={CardData} cardIndex="0" />}
-                {active === "CardTwo" && <Card CardData={CardData} cardIndex="1" />}
-                {active === "CardThree" && <Card CardData={CardData} cardIndex="2" />}
-                {active === "CardFour" && <Card CardData={CardData} cardIndex="3" />}
+              {active === "CardOne" && <Card CardData={CardData} cardIndex="0" />}
+              {active === "CardTwo" && <Card CardData={CardData} cardIndex="1" />}
+              {active === "CardThree" && <Card CardData={CardData} cardIndex="2" />}
+              {active === "CardFour" && <Card CardData={CardData} cardIndex="3" />}
             </div>
         </div>
       </div>
