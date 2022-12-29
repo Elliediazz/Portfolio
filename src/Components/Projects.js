dc2b9c684a91ebaa315a8ee3c44fef1228bf07be
Ellie Barrera <elliediaz1204@gmail.com>
2022-12-29 11:38:32 -0500

    Added github icon to projects'

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,7 @@ export default function Projects() {
         <div className="project-container">
           <div className="project">
             <h2 className="project-title">Where to Eat?</h2>
-            <a className="github-link" target="_blank" rel="noopener noreferrer" href= "https://github.com/Elliediazz/JediBook-Client" >Github Link</a>
+            <a className='icon' href="https://github.com/Elliediazz/Final-Project" target="_blank" rel="noreferrer" class="nav-item nav-link"><ion-icon size="large" name="logo-github"></ion-icon></a>
             <h3 className="project-discription">
               Collaborated with a team to develop an application that allows users to search for restaurants in their area and randomizes the results to choose one when prompted. This application displays real-time data using the google API but only after the user grants permission.
             </h3>
@@ -21,18 +21,17 @@ export default function Projects() {
 
           <div className="project">
             <h2 className="project-title">Sith World</h2>
-            <a className="github-link" target="_blank" rel="noopener noreferrer" href= "https://github.com/Elliediazz/JediBook-Client" >Github Link</a>
+            <a className='icon' href="https://github.com/Elliediazz/JediBook-Client" target="_blank" rel="noreferrer" class="nav-item nav-link"><ion-icon size="large" name="logo-github"></ion-icon></a>
             <h3 className="project-discription">
-              An app that allows users to search through the Planets, Characters, and Starships associated with the Star Wars saga. Users can also create their own Avatars and maintain their characters through their personal accounts. Built using the Star Wars API.
+              An app that allows users to search through the Planets, Characters, and Starships associated with the Star Wars saga. Users can also create their own Avatars and maintain their characters through their personal accounts. Built using the Star Wars API. (Still in Progress)
             </h3>
-            <h2 className="in-progress">In Progress</h2>
             <h4 className="built-with">
               Technologies Used: HTML, CSS, Javascript, React.js, Express, Node.js, Mongoose, Mongo DB, SWAPI
             </h4>
           </div>
           <div className="project">
             <h2 className="project-title">CDEV Quizzes</h2>
-            <a className="github-link" target="_blank" rel="noopener noreferrer" href= "https://github.com/Elliediazz/CDEV-Quizzes-Client" >Github Link</a>
+            <a className='icon' href="https://github.com/Elliediazz/CDEV-Quizzes-Client" target="_blank" rel="noreferrer" class="nav-item nav-link"><ion-icon size="large" name="logo-github"></ion-icon></a>
             <h3 className="project-discription">
               This application aimed to develop a Quizzing platform using the React Framework and Mongo Database. Our application allows users to create new quizzes upon request, list all quizzes on the platform, allow the user to insert new questions to any chosen quiz, and allow the user to update and delete quizzes - In Collaboration with three other developers.
             </h3>
